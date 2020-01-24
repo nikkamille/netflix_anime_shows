8601413913ef5edf3bbeebd93604b7bb30a616fa
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-24 22:53:15 +0000

    created a class variable that is equal to an empty array

--- lib/netflix_anime_shows/anime.rb
+++ lib/netflix_anime_shows/anime.rb
@@ -3,4 +3,11 @@ class Anime
 # Create setters and getters for :title, :description, :release_year, and :no_of_seasons
   attr_accessor :title, :description, :release_year, :no_of_seasons
   
+# Create an empty array that will contain all of the anime shows as they are instantiated.
+  @@all = []
+  
+  def initialize
+    @@all << self
+  end
+  
 end
