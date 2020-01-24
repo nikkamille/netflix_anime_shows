203f57a5ae708295dfdd0f252816bd8bdb1c7b17
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-24 22:47:46 +0000

    created setters and getters for instance methods

---
+++ lib/netflix_anime_shows/anime.rb
@@ -0,0 +1,6 @@
+class Anime
+  
+# Create setters and getters for :title, :description, :release_year, and :no_of_seasons
+  attr_accessor :title, :description, :release_year, :no_of_seasons
+  
+end
