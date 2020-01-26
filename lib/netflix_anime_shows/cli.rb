ee6f499d29546248395efd463393e821572a154a
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-26 09:54:53 +0000

    adding methods to the CLI class

--- lib/netflix_anime_shows/cli.rb
+++ lib/netflix_anime_shows/cli.rb
@@ -1,11 +1,19 @@
 class NetflixAnimeShows::CLI 
   
   def start 
-    puts "Kon'nichiwa! Here's our list of anime shows in Netflix."
-    # anime_list
+    anime_list
+    menu
   end
   
+  def anime_list
+    puts ""
+    puts "Kon'nichiwa! Here's our list of anime shows in Netflix."
+    puts ""
+  end
   
-  
+  def menu
+    puts "Which anime would you like to know about?"
+    user_input = gets.chomp.to_i 
+  end
   
 end
