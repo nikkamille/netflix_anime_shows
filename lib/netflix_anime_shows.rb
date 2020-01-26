fa1401c8fbe0e0b16d0b18ad9a09008806189d9a
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-26 09:17:21 +0000

    got the bin file to work

--- lib/netflix_anime_shows.rb
+++ lib/netflix_anime_shows.rb
@@ -1,5 +1,8 @@
 require_relative './netflix_anime_shows/version'
+require_relative './netflix_anime_shows/anime'
 require_relative './netflix_anime_shows/cli'
+require_relative './netflix_anime_shows/scraper'
+
 
 module NetflixAnimeShows
   class Error < StandardError; end
