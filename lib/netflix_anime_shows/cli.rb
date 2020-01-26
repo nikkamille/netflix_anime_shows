c351545f7167ae2c1fe00f83cdf6a1241287be6a
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-26 06:18:20 +0000

    defined methods in the Scraper class

---
+++ lib/netflix_anime_shows/cli.rb
@@ -0,0 +1,11 @@
+class NetflixAnimeShows::CLI 
+  
+  def start 
+    puts "Kon'nichiwa! Here's our list of anime shows in Netflix."
+    anime_list
+  end
+  
+  
+  
+  
+end
--- lib/netflix_anime_shows/scraper.rb
+++ lib/netflix_anime_shows/scraper.rb
@@ -1,12 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
+# require 'pry'
 
 require_relative './anime.rb'
 
-# Create a Scraper class.
-
 class Scraper
+  
+  def scrape_titles
+    document = NOKOGIRI::HTML(open("https://www.netflix.com/browse/genre/7424"))
+    # titles = document.css()
+  end
+  
+  def anime_list # returns all the titles as a list
+    
+  end
+  
   
   
 end
