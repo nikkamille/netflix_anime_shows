1d5dfd2cd538494a101750cec1820e110f5e24fb
Kamille Longalong <k.pagibigan@gmail.com>
2020-01-29 07:08:19 +0000

    modified cli class

--- lib/netflix_anime_shows/cli.rb
+++ lib/netflix_anime_shows/cli.rb
@@ -1,29 +1,44 @@
 class NetflixAnimeShows::CLI 
   
-  def start 
-    anime_list
+  def start
+    greeting
     menu
-    sayonara
+    more_info
   end
   
-  def anime_list
+  def greeting
     puts ""
-    puts "Kon'nichiwa! Would you like to see a list of anime shows in Netflix? Type in yes or no."
+    puts "Kon'nichiwa! n_n"
     puts ""
-    
-    @anime = NetflixAnimeShows::Anime.all
-    
-    user_input = gets.chomp.downcase
-    if user_input == "yes"
-      puts  
-    end
-        
-    
+  end
+  
+  def anime_list # method that shows all the titles alphabetically and with corresponding numbers
+  
   end
   
   def menu
-    puts "Which anime would you like to know about? Enter a number:"
+    puts ""
+    puts "Would you like to see our list of anime shows in Netflix? Type in yes or no."
+    puts ""
+    
+    # @anime = NetflixAnimeShows::Anime.all # will show all the titles alphabetically and with corresponding numbers
+    
+    user_input = get.chomp.downcase
+    case user_input
+    when "yes"
+      puts "all the anime titles" # anime_list
+    when "no"
+      sayonara
+    else
+      puts "I'm sorry; I don't know that."
+      menu
+    end
+  end
+  
+  def more_info
+    puts "Which anime would you like to know more about? Enter a number:"
     user_input = gets.chomp.to_i 
+    # should be able to choose an anime from the list by typing in a number and give out more info
   end
   
   def sayonara
