7877765c54e6f7ee7593f58c47517c3052ad42cc
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-14 04:38:11 +0000

    added the required files

--- lib/netflix_anime_shows.rb
+++ lib/netflix_anime_shows.rb
@@ -1,10 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 require_relative './netflix_anime_shows/version'
 require_relative './netflix_anime_shows/anime'
 require_relative './netflix_anime_shows/cli'
 require_relative './netflix_anime_shows/scraper'
-
-
-module NetflixAnimeShows
-  class Error < StandardError; end
-  # Your code goes here...
-end
