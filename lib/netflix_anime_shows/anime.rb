e4e8bfce5ab57a4e2701c0e66ffa1902b9a08a97
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-15 07:00:42 +0000

    created scrape method

--- lib/netflix_anime_shows/anime.rb
+++ lib/netflix_anime_shows/anime.rb
@@ -1,8 +1,13 @@
 class NetflixAnimeShows::Anime
   
-  attr_accessor :title, :url, :description, :release_year, :no_of_seasons
+  attr_accessor :title, :description, :release_year, :no_of_seasons
   
   @@all = []
   
+  def scrape_anime_page
+    doc = NOKOGIRI::HTML(open("https://www.netflix.com/browse/genre/7424"))
+    titles = doc.css(".nm-collections-title-name").text
+    binding.pry 
+  end
   
 end
