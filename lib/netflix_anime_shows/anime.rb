8069b8d2d569a0ef7efee674dbc1a5262577405d
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-15 23:23:31 +0000

    added an initialize method

--- lib/netflix_anime_shows/anime.rb
+++ lib/netflix_anime_shows/anime.rb
@@ -4,6 +4,13 @@ class NetflixAnimeShows::Anime
   
   @@all = []
   
+  def initialize(title:, description:, release_year:, no_of_seasons:)
+    @title = title
+    @description = description
+    @release_year = release_year
+    @no_of_seasons = no_of_seasons
+  end
+  
   def scrape_anime_page
     doc = NOKOGIRI::HTML(open("https://www.netflix.com/browse/genre/7424"))
     titles = doc.css(".nm-collections-title-name").text
