c09b6f5f1a5c3919250073e56d7e09a055d32629
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-15 23:38:37 +0000

    fixed uninitialized constant NameError

--- lib/netflix_anime_shows/anime.rb
+++ lib/netflix_anime_shows/anime.rb
@@ -1,4 +1,6 @@
-class NetflixAnimeShows::Anime
+require 'pry'
+
+class Anime
   
   attr_accessor :title, :description, :release_year, :no_of_seasons
   
@@ -9,7 +11,10 @@ class NetflixAnimeShows::Anime
     @description = description
     @release_year = release_year
     @no_of_seasons = no_of_seasons
+    binding.pry
   end
+  
+  Anime.new(title: "One Punch Man", description: "kkkk", release_year: "2015", no_of_seasons: "2") 
   
   def scrape_anime_page
     doc = NOKOGIRI::HTML(open("https://www.netflix.com/browse/genre/7424"))
