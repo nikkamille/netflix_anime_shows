53526bd013ec80c8adb7ee5bf43410603a097f3f
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-15 07:48:30 +0000

    added info to the gemspec file

--- lib/netflix_anime_shows/anime.rb
+++ lib/netflix_anime_shows/anime.rb
@@ -7,7 +7,7 @@ class NetflixAnimeShows::Anime
   def scrape_anime_page
     doc = NOKOGIRI::HTML(open("https://www.netflix.com/browse/genre/7424"))
     titles = doc.css(".nm-collections-title-name").text
-    binding.pry 
+    # binding.pry 
   end
   
 end
