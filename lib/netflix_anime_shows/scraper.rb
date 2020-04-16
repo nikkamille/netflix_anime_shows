ec721293f6d8948cc69dfb93dd079db2e508acf0
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-16 00:45:11 +0000

    added Scraper class

--- lib/netflix_anime_shows/scraper.rb
+++ lib/netflix_anime_shows/scraper.rb
@@ -7,9 +7,13 @@ require_relative './anime.rb'
 class Scraper
   
   def scrape_anime_page
-    doc = NOKOGIRI::HTML(open("https://www.netflix.com/browse/genre/7424"))
+    doc = Nokogiri::HTML(open("https://www.netflix.com/browse/genre/7424"))
     # titles = doc.css(".nm-collections-title-name").text
     binding.pry 
   end
   
 end
+
+Scraper.new.scrape_anime_page
+
+# title = doc.css(".nm-collections-title-name").first.text
