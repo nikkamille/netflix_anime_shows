9f79b5597157825c0451d51b5152480122346844
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-16 05:34:50 +0000

    defined instance methods

--- lib/netflix_anime_shows/scraper.rb
+++ lib/netflix_anime_shows/scraper.rb
@@ -6,15 +6,22 @@ require_relative './anime.rb'
 
 class Scraper
   
-  def scrape_page 
+  def get_page 
     doc = Nokogiri::HTML(open("https://www.netflix.com/browse/genre/7424"))
-    # binding.pry 
   end
   
-  def get_details
+  def scrape_anime_shows
+    self.get_page.css(".nm-collections-title-name").text
+  end
+  
+  def create_anime_shows
+    scrape_anime_shows.each do |anime|
+      Anime.new
+    end
+  end
   
 end
 
-Scraper.new.scrape_page
+Scraper.new.scrape_anime_shows
 
 # title = doc.css(".nm-collections-title-name").first.text
