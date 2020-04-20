d2a6baa437906a9bf9dec7f130cafbdee1fffe51
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-20 02:20:16 +0000

    deleted methods

--- lib/netflix_anime_shows/scraper.rb
+++ lib/netflix_anime_shows/scraper.rb
@@ -8,19 +8,10 @@ class Scraper
   
   def get_page 
     anime_page = Nokogiri::HTML(open("https://www.whats-on-netflix.com/whats-new/anime/"))
-    binding.pry
   end
   
-  def scrape_anime_shows
-    self.get_page.css(".nm-collections-title-name").text
-  end
-  
-  def scrape_anime_info
-    self.get_info.css(".details-container")
-  end
-  
-  def create_anime_shows
-    scrape_anime_shows.each do |anime|
+  def create_anime
+    get_page.each do |anime|
       Anime.new
     end
   end
