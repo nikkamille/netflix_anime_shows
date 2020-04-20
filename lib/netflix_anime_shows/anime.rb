6c0c2f5d15227d9c25f26ddf5ece391e7454f5b3
Kamille Longalong <k.pagibigan@gmail.com>
2020-04-20 09:46:43 +0000

    added methods in anime.rb file

--- lib/netflix_anime_shows/anime.rb
+++ lib/netflix_anime_shows/anime.rb
@@ -1,18 +1,41 @@
+require 'pry'
+
+require_relative '../netflix_anime_shows'
+
 class Anime
   
-  attr_accessor :title, :description, :release_year, :no_of_seasons
+  attr_accessor :title, :description, :rating
   
   @@all = []
   
   def initialize
-    @all << self
+    @title = title
+    @description = description
+    @rating = rating
+    @@all << self
+  end
+  
+  def get_page 
+    doc = Nokogiri::HTML(open("https://www.whats-on-netflix.com/whats-new/anime/"))
+  end
+  
+  def create_anime
+    get_page.css(".new-title").each do |anime|
+      anime = Anime.new
+      anime.title = get_page.css(".new-title-right h5").text
+      anime.description = get_page.css(".title-description").text
+      anime.rating = get_page.css(".new-title-ratings").text.gsub(/\s+/, '')
+    end
+    binding.pry
   end
   
   def self.all
-    @all
+    @@all
   end
   
   def self.reset_all
-    @all.clear
+    @@all.clear
   end
 end
+
+Anime.new.create_anime
